b2b23f371ab5d1759cb7ebf8a4dc8a59cd4b75b3
chentsu <wox.chentsu@gmail.com>
2016-10-25 13:23:31 +0300

    логирование

--- coinGame.js
+++ coinGame.js
@@ -2,7 +2,8 @@
  * Created by chentsu on 24.10.2016.
  */
 
-var argc = process.argv.length;
+var doLog = false;
+// var argc = process.argv.length;
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 var readline = require('readline');
@@ -12,19 +13,26 @@ var rl=readline.createInterface({
 });
 
 if ( argv.help /*|| argc<2*/) {
-    rl.write('Usage:\t node coinGame.js [LogFile]\nWhere:\tLogFile - path to log file.');
+    rl.write('\tUsage:\tnode coinGame.js [LogFile]\n\tWhere:\tLogFile - path to log file.');
+    rl.close();
 }
 else {
+    if(argv._[0]) doLog=true;
+
     rl.write('Угадайте какой стороной упадёт монета.\nВведите 1(орёл) или 2(решка). 0 - выход из программы.\n');
 
     rl.on('line', function (inpt) {
-        var coin= randCoin();
+        var coin=1+Math.round(Math.random());
         inpt = parseInt(inpt);
-        if (inpt===1 || inpt===2) {
+        if (inpt===1 || inpt===2)
+        {
             if (inpt === coin)
                 console.log('Вы угадали!');
             else
                 console.log('Вы не угадали.');
+
+            if ( doLog )
+                fs.appendFile(argv._[0],coin+','+inpt+','+(inpt===coin)+',',function (err) {if (err) throw 'Не могу открыть для записи файл: '+err;});
         }
 
         if (inpt===0)
@@ -32,8 +40,4 @@ else {
         else
             console.log('\nВведите 1(орёл) или 2(решка). 0 - выход из программы.\n');
     });
-}
-
-function randCoin() {
-    return 1+Math.round(Math.random());
 }
