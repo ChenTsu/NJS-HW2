40b84f253f9fede5b40b1a8595f388bb09335510
chentsu <wox.chentsu@gmail.com>
2016-10-27 11:47:25 +0300

    blackjack log

--- blackJack.js
+++ blackJack.js
@@ -94,60 +94,63 @@ function dealerMoreCards () // добавляем карты если нужно
 /*************************************************************************
 						тело программы
 *************************************************************************/
-// получаем первые карты
-var dealerCards = [];
-var playerCards = [];
-var moreGame=true;
-var dealerSum=0;
-var playerSum=0;
-
-console.log('\nИгра BlackJack "21" приветствует Вас.\n');
-
-while ( moreGame )
-{
-	dealerCards = [getCard()];
-	playerCards = [getCard(),getCard()];
-	dealerSum=0;
-	playerSum=0;
-	
-	playerMoreCards();		//		добавляем карты игроку	//
-	playerSum=getSumCards(playerCards);	// считаем суммы карт игрока, а то вызовы функий слишком громоздки в записи
-
-	if ( playerSum > 21)
-	{
-		console.log ( handStatus()+"\nСожалеем. Вы проиграли! У вас перебор."  );
-	}
-	else
-	{
-		// если у игрока сумма норм, добавляем карты дилеру //
-		dealerMoreCards ();
-		dealerSum=getSumCards(dealerCards); // считаем суммы карт дилера, а то вызовы функий слишком громоздки в записи
-
-
-		if ( dealerSum > 21) // мы уже проверили что у игрока сумма норм
-		{
-			console.log ( handStatus()+"\nПоздравляем! Вы выиграли! У дилера перебор." );
-		}
-		else if ( playerSum==21 && playerCards.length==2)
-		{
-			console.log ( handStatus()+"\nПоздравляем! У вас BlackJack! Вы выиграли!" );
-		}
-		else if ( playerSum > dealerSum  )
-		{
-			console.log ( handStatus()+"\nПоздравляем! Вы выиграли!" );
-		}
-		else if ( playerSum < dealerSum )
-		{
-			console.log ( handStatus()+"\nСожалеем. Вы проиграли!" );
-		}
-		else
-		{
-			console.log ( handStatus()+"\nОчков поровну. Никто не выиграл." );
-		}
-	}
-
-	moreGame= 1===Number(syncprompt("Хотите играть ещё раз? (1 - играть ещё, что-либо другое - нет). "));
+if ( argv.help /*|| argc<2*/) {
+	rl.write('\tUsage:\tnode blackJack.js [LogFile]\n\tWhere:\tLogFile - path to log file.');
+	rl.close();
 }
+else {
+	// получаем первые карты
+	var dealerCards = [];
+	var playerCards = [];
+	var moreGame = true;
+	var dealerSum = 0;
+	var playerSum = 0;
 
-console.log ( 'Игра BlackJack "21"\nДо встречи. ');
-process.exit();
+	console.log('\nИгра BlackJack "21" приветствует Вас.\n');
+
+	while (moreGame) {
+		dealerCards = [getCard()];
+		playerCards = [getCard(), getCard()];
+		dealerSum = 0;
+		playerSum = 0;
+
+		playerMoreCards();		//		добавляем карты игроку	//
+		playerSum = getSumCards(playerCards);	// считаем суммы карт игрока, а то вызовы функий слишком громоздки в записи
+
+		if (playerSum > 21) {
+			console.log(handStatus() + "\nСожалеем. Вы проиграли! У вас перебор.");
+		}
+		else {
+			// если у игрока сумма норм, добавляем карты дилеру //
+			dealerMoreCards();
+			dealerSum = getSumCards(dealerCards); // считаем суммы карт дилера, а то вызовы функий слишком громоздки в записи
+
+
+			if (dealerSum > 21) // мы уже проверили что у игрока сумма норм
+			{
+				console.log(handStatus() + "\nПоздравляем! Вы выиграли! У дилера перебор.");
+			}
+			else if (playerSum == 21 && playerCards.length == 2) {
+				console.log(handStatus() + "\nПоздравляем! У вас BlackJack! Вы выиграли!");
+			}
+			else if (playerSum > dealerSum) {
+				console.log(handStatus() + "\nПоздравляем! Вы выиграли!");
+			}
+			else if (playerSum < dealerSum) {
+				console.log(handStatus() + "\nСожалеем. Вы проиграли!");
+			}
+			else {
+				console.log(handStatus() + "\nОчков поровну. Никто не выиграл.");
+			}
+		}
+		if(argv._[0]) {
+			if ( fs.statSync(argv._[0]).size>0 )
+				fs.appendFileSync(argv._[0],',');
+			fs.appendFileSync(argv._[0], dealerCards.join(' ') + ',' + playerCards.join(' ') /*,function (err) {if (err) throw 'Не могу открыть для записи файл: '+err;}*/);
+		}
+		moreGame = 1 === Number(syncprompt("\nХотите играть ещё раз? (1 - играть ещё, что-либо другое - нет). "));
+	}
+
+	console.log('Игра BlackJack "21"\nДо встречи. ');
+	process.exit();
+}
